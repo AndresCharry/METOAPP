5e36598e4975bd0f55d4daf74601199c4470436a
Andres Charry <ingeniero.andres.charry@gmail.com>
2022-09-20 17:08:08 -0500

    cambios en coordenadas para pedir mas informacion

--- Coordenadas/views.py
+++ Coordenadas/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+import pandas as pd
 
 from .forms import FormularioCoordenadas
 from .upload import  handle_uploaded_file
@@ -7,7 +8,14 @@ from .baseDeDatos.baseDeDatos import  base_de_datos
 
 def coordenadas(request):
     formulario_coordenadas = FormularioCoordenadas()
-
+    url = pd.read_csv('/home/charry/Documents/programacion/trabajo de grado/web/ProjectWeb/BaseDatos/BaseDeDatos/proyecto.csv')
+    url = url.at[0,'url']
+    df = pd.read_csv(url)
+    try:
+        medicion_manual = df.at[0,'Medicion manual']
+    except:
+        medicion_manual = False
+ 
     if request.method == "POST":
         formulario_coordenadas = FormularioCoordenadas(request.POST,request.FILES)
 
@@ -15,9 +23,13 @@ def coordenadas(request):
             # creaccion del diccionario
             datos = {}
             datos['Tipo de cultivo'] = request.POST.get("tipo_cultivo")
+            if medicion_manual != True:
+                datos['Numero de marcadores'] = request.POST.get("numero_de_marcadores")
+            else:
+                datos['Numero de variables'] = request.POST.get("n_variables")
             url_coordenadas = handle_uploaded_file(request.FILES['file'])
             base_de_datos(url_coordenadas, datos)
             return redirect("/coordenadas/?valido")
 
-    return render(request, "Coordenadas/coordenadas.html", {'coordenadas': formulario_coordenadas})
+    return render(request, "Coordenadas/coordenadas.html", {'coordenadas': formulario_coordenadas, 'manual': medicion_manual})
 
